7ef082b76545f8f8561ce6461a01b5e016081eb5
GayathriYantra <109575261+GayathriYantra@users.noreply.github.com>
2022-07-26 13:07:00 +0530

    Update CreateNewProject.java

--- Github/src/test/java/com/mavenproject/CreateNewProject.java
+++ Github/src/test/java/com/mavenproject/CreateNewProject.java
@@ -5,6 +5,7 @@ public class CreateNewProject {
 	public static void main(String[] args) 
 	{
 		// modification happened
+		// modification happened twise
 		System.out.println("Create new Repository");
 		System.out.println("modification done");
 	}
